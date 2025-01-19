0aa85a8fa40182ebcbb6d071c4de8c2e77df2525
vachg <vachgen@yandex.ru>
2025-01-19 18:06:27 +0300

    Start

---
+++ src/main/java/pro_sky_complex_version/Coursework2QuestionGeneratorComplexVersion/Coursework2QuestionGeneratorComplexVersionApplication.java
@@ -0,0 +1,13 @@
+package pro_sky_complex_version.Coursework2QuestionGeneratorComplexVersion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Coursework2QuestionGeneratorComplexVersionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Coursework2QuestionGeneratorComplexVersionApplication.class, args);
+	}
+
+}
